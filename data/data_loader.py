5dc1f0a221e66a4adbd3c9235c994b821a7a9273
Jack Pamukci <jackpamukci824@gmail.com>
2024-07-30 13:06:59 -0400

    wip: adding possession sequences

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,4 +1,6 @@
 import soccerdata as sd
+
+# import socceraction.spadl as spadl
 import pandas as pd
 from tqdm import tqdm
 import logging
@@ -22,6 +24,7 @@ class HistoricData:
         self,
         season_id: int,
         league_id: str,
+        num_games: int = 380,
         env_path: str = None,
         bucket: str = "footballbets",
     ):
@@ -38,13 +41,15 @@ class HistoricData:
         self.season_id: int = season_id
         self.league_id: str = league_id
 
-        self.ws = sd.WhoScored(leagues=self.league_id, seasons=self.season_id)
-
         self.fbref = sd.FBref(leagues=self.league_id, seasons=self.season_id)
+
+        self.ws = sd.WhoScored(leagues=self.league_id, seasons=self.season_id)
 
         self.mh = sd.MatchHistory(leagues=self.league_id, seasons=self.season_id)
 
         self.understat = sd.Understat(leagues=self.league_id, seasons=self.season_id)
+
+        self.schedule = self._get_master_schedule().iloc[:num_games]
 
         self._load_event_data()
         self._load_missing_players()
@@ -54,31 +59,33 @@ class HistoricData:
         self._load_odds()
 
     def _load_event_data(self):
-        event_data = self.ws.read_events(output_fmt="spadl")
+        event_data = self.ws.read_events(list(self.schedule.ws_game_id))
+
+        # event_data = spadl.add_names(event_data)
 
         logging.info("SPADL Data Loaded")
 
         spadl_buffer = StringIO()
 
-        event_data.to_csv(spadl_buffer, index=False)
+        event_data.to_csv(spadl_buffer, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/events/events_spadl.csv",
+            Key=f"{self.league_id}/{self.season_id}/events_spadl.csv",
             Body=spadl_buffer.getvalue(),
         )
 
         logging.info("SPADL Data Into S3")
 
     def _load_missing_players(self):
-        missing_players = self.ws.read_missing_players()
+        missing_players = self.ws.read_missing_players(list(self.schedule.ws_game_id))
 
         logging.info("Missing Player Data Loaded")
 
         players_buffer = StringIO()
-        missing_players.to_csv(players_buffer, index=False)
+        missing_players.to_csv(players_buffer, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/missing_players/missing_players.csv",
+            Key=f"{self.league_id}/{self.season_id}/missing_players.csv",
             Body=players_buffer.getvalue(),
         )
 
@@ -86,15 +93,17 @@ class HistoricData:
 
     def _load_player_match_stats(self):
 
-        player_match_data = self.understat.read_player_match_stats()
+        player_match_data = self.understat.read_player_match_stats(
+            list(self.schedule.und_game_id)
+        )
 
         logging.info("Player Data Loaded")
 
         players_match = StringIO()
-        player_match_data.to_csv(players_match, index=False)
+        player_match_data.to_csv(players_match, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/player_match_stats/player_stats.csv",
+            Key=f"{self.league_id}/{self.season_id}/player_stats.csv",
             Body=players_match.getvalue(),
         )
 
@@ -102,30 +111,32 @@ class HistoricData:
 
     def _load_lineups(self):
 
-        lineups = self.fbref.read_lineup()
+        lineups = self.fbref.read_lineup(list(self.schedule.fbref_game_id))
 
         logging.info("Lineup Data Loaded")
 
         lineups_match = StringIO()
-        lineups.to_csv(lineups_match, index=False)
+        lineups.to_csv(lineups_match, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/lineups/lineups.csv",
+            Key=f"{self.league_id}/{self.season_id}/lineups.csv",
             Body=lineups_match.getvalue(),
         )
 
         logging.info("Lineup Data Into S3")
 
     def _load_team_match_data(self):
-        team_match_data = self.understat.read_team_match_stats()
+        team_match_data = self.understat.read_team_match_stats(
+            list(self.schedule.und_game_id)
+        )
 
         logging.info("Team Data Loaded")
 
         team_match = StringIO()
-        team_match_data.to_csv(team_match, index=False)
+        team_match_data.to_csv(team_match, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/team_match_stats/team_stats.csv",
+            Key=f"{self.league_id}/{self.season_id}/team_stats.csv",
             Body=team_match.getvalue(),
         )
 
@@ -137,14 +148,50 @@ class HistoricData:
         logging.info("Odds Data Loaded")
 
         odds_match = StringIO()
-        odds_data.to_csv(odds_match, index=False)
+        odds_data.to_csv(odds_match, index=True)
         self.s3.put_object(
             Bucket=self.bucket,
-            Key=f"{self.league_id}/{self.season_id}/odds_data/odds.csv",
+            Key=f"{self.league_id}/{self.season_id}/odds.csv",
             Body=odds_match.getvalue(),
         )
 
         logging.info("Team Match Stats Into S3")
+
+    def _get_master_schedule(self):
+        epl_schedule = self.ws.read_schedule()
+        fbref_schedule = self.fbref.read_schedule()
+        understat_schedule = self.understat.read_schedule()
+
+        master_schedule = epl_schedule.merge(
+            fbref_schedule[["game_id"]], left_index=True, right_index=True, how="inner"
+        )
+        master_schedule = master_schedule.merge(
+            understat_schedule[["game_id"]],
+            left_index=True,
+            right_index=True,
+            how="left",
+        )
+
+        master_schedule.rename(
+            columns={
+                "game_id_x": "ws_game_id",
+                "game_id_y": "fbref_game_id",
+                "game_id": "und_game_id",
+            },
+            inplace=True,
+        )
+
+        master_schedule = master_schedule.reset_index().set_index("game")
+
+        schedule_match = StringIO()
+        master_schedule.to_csv(schedule_match, index=True)
+        self.s3.put_object(
+            Bucket=self.bucket,
+            Key=f"{self.league_id}/{self.season_id}/schedule.csv",
+            Body=schedule_match.getvalue(),
+        )
+
+        return master_schedule
 
     def _create_session(self, env_path: str):
 
@@ -210,20 +257,33 @@ def main_menu(stdscr, options):
             int_code = int(int_code.decode("utf-8"))  # Decode and convert to integer
 
             stdscr.addstr(0, 0, f"You entered: {int_code}")
-            return selected_option, int_code
+            stdscr.refresh()
+
+            stdscr.addstr(15, 0, "Enter number of games: ")
+            curses.echo()  # Enable echoing of typed characters
+            num_games = stdscr.getstr(
+                16, 0, 20
+            )  # Get user input, max length of 20 characters
+            num_games = int(num_games.decode("utf-8"))
+
+            return selected_option, int_code, num_games
 
 
 def main():
 
     # Initialize curses and show the menu
-    selected_league, selected_season = curses.wrapper(main_menu, supported_leagues)
+    selected_league, selected_season, num_games = curses.wrapper(
+        main_menu, supported_leagues
+    )
     print(f"You selected: {selected_league}")
 
     # Ask for an integer input
     print(f"You entered: {selected_season}")
 
     print("Starting web scraper...")
-    HistoricData(season_id=selected_season, league_id=selected_league)
+    HistoricData(
+        season_id=selected_season, league_id=selected_league, num_games=num_games
+    )
 
 
 if __name__ == "__main__":
