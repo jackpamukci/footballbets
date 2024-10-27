d77d3d247a98aa877bbd173ba862429bbb3bdb4f
jackpamukci <jackpamukci824@gmail.com>
2024-10-26 21:54:52 -0400

    wip: hybrid model dataset

--- dataset/torch_dataset.py
+++ dataset/torch_dataset.py
@@ -6,14 +6,14 @@ import numpy as np
 import pandas as pd
 
 
-class ModelDataset(Dataset):
+class PlayerDataset(Dataset):
     def __init__(
         self,
         player_features: pd.DataFrame,
         schedule: pd.DataFrame,
         device: torch.device = torch.device("cpu"),
     ):
-        super(ModelDataset, self).__init__()
+        super(PlayerDataset, self).__init__()
         self.features = player_features
         self.schedule = schedule
 
@@ -31,6 +31,136 @@ class ModelDataset(Dataset):
         self.features = self.features[self.features["lookback"] != 1]
 
         player_feats = []
+        targets = []
+        player_config = []
+        targets = []
+        config = []
+
+        groups = self.features.groupby("game")
+        for game, group in groups:
+
+            home_team = group[group["h_a"] == "home"].sort_values(
+                "lookback_minutes", ascending=False
+            )
+            away_team = group[group["h_a"] == "away"].sort_values(
+                "lookback_minutes", ascending=False
+            )
+
+            home_feats = (
+                home_team.drop(self.player_config_cols, axis=1)
+                .iloc[:14]
+                .values.astype(float)
+            )
+            home_config = home_team[self.player_config_cols].iloc[:14]
+
+            away_feats = (
+                away_team.drop(self.player_config_cols, axis=1)
+                .iloc[:14]
+                .values.astype(float)
+            )
+            away_config = away_team[self.player_config_cols].iloc[:14]
+
+            # max_rows = max(home_feats.shape[0], away_feats.shape[0])
+            # max_cols = max(home_feats.shape[1], away_feats.shape[1])
+            home_feats_padded = np.pad(
+                home_feats,
+                (
+                    (0, 14 - home_feats.shape[0]),
+                    (0, 30 - home_feats.shape[1]),
+                ),
+                mode="constant",
+                constant_values=0,
+            )
+            away_feats_padded = np.pad(
+                away_feats,
+                (
+                    (0, 14 - away_feats.shape[0]),
+                    (0, 30 - away_feats.shape[1]),
+                ),
+                mode="constant",
+                constant_values=0,
+            )
+
+            stacked_array = np.stack([home_feats_padded, away_feats_padded], axis=0)
+            player_tensor = torch.tensor(stacked_array, dtype=torch.float)
+
+            player_config_df = pd.concat([home_config, away_config])
+            sched_match = self.schedule[self.schedule["game"] == game].iloc[0]
+
+            target = 0 if sched_match.home_score == sched_match.away_score else (1 if sched_match.home_score > sched_match.away_score else 0)
+
+            player_feats.append(player_tensor)
+
+            target = (
+                0
+                if sched_match.home_score == sched_match.away_score
+                else (1 if sched_match.home_score > sched_match.away_score else 2)
+            )
+
+            targets.append(target)
+            player_config.append(player_config_df)
+            config.append(
+                {
+                    "fixture": game,
+                    "season": sched_match.season,
+                    "league": sched_match.league,
+                }
+            )
+
+        self.data = {
+            "player_features": player_feats,
+            "targets": targets,
+            # "player_info": player_config,
+            "config": config,
+        }
+
+    def __len__(self):
+        return len(self.data["player_features"])
+
+    def __getitem__(self, idx: int):
+        return (
+            self.data["player_features"][idx],
+            self.data["targets"][idx],
+            # self.data["player_info"][idx],
+            self.data["config"][idx],
+        )
+    
+
+
+class HybridDataset(Dataset):
+    def __init__(
+        self,
+        team_features: pd.DataFrame,
+        player_features: pd.DataFrame,
+        schedule: pd.DataFrame,
+        device: torch.device = torch.device("cpu"),
+    ):
+        super(HybridDataset, self).__init__()
+        self.player_features = player_features
+        self.team_features = team_features
+        self.schedule = schedule
+
+        self.player_config_cols = [
+            "league",
+            "season",
+            "game",
+            "team",
+            "player",
+            "h_a",
+            "lookback",
+            "matchday",
+        ]
+
+        self.team_config_cols = ['season', 'game', 'date', 'home_team', 'away_team', 'target',
+       'matchday', 'lookback']
+
+        self.team_features = self.team_features[self.team_features["lookback"] != 1]
+        self.player_features = self.player_features[self.player_features["lookback"] != 1]
+
+        ## TODO: assert that all the games in player dataset, event dataset and schedule match
+
+        player_feats = []
+        team_feats = []
         targets = []
         player_config = []
         targets = []
--- models/utils.py
+++ models/utils.py
@@ -1,8 +1,10 @@
 import torch
+import torch.nn.functional as F
 from sklearn.metrics import confusion_matrix, precision_recall_curve
 from sklearn.preprocessing import label_binarize
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def train_routine(
@@ -15,80 +17,80 @@ def train_routine(
 ):
     # model.to(device)
 
-  train_losses = []
-  train_acc = []
-  test_losses = []
-  test_acc = []
+    train_losses = []
+    train_acc = []
+    test_losses = []
+    test_acc = []
 
-  for epoch in range(num_epochs):
+    for epoch in range(num_epochs):
 
-      # Training phase
-      model.train()  # Set model to training mode
-      running_loss = 0.0
-      running_corrects = 0
-      total = 0
+        # Training phase
+        model.train()  # Set model to training mode
+        running_loss = 0.0
+        running_corrects = 0
+        total = 0
 
-      for batch in train_loader:
-          inputs, labels, _ = batch
-          optimizer.zero_grad()  # Zero the gradients
+        for batch in train_loader:
+            inputs, labels, _ = batch
+            optimizer.zero_grad()  # Zero the gradients
 
-          outputs = model(inputs)
-          probabilities = F.softmax(outputs, dim=1)
-          _, preds = torch.max(probabilities, 1)
-          loss = loss_fn(outputs, labels)
+            outputs = model(inputs)
+            probabilities = F.softmax(outputs, dim=1)
+            _, preds = torch.max(probabilities, 1)
+            loss = loss_fn(outputs, labels)
 
-          # Backward pass and optimization
-          loss.backward()
-          optimizer.step()
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
 
-          # Statistics
-          running_loss += loss.item() * inputs.size(0)
-          running_corrects += torch.sum(preds == labels.data)
-          total += labels.size(0)
+            # Statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+            total += labels.size(0)
 
-      # Calculate accuracy and loss for the epoch
-      epoch_loss = running_loss / total
-      epoch_acc = running_corrects.double() / total
+        # Calculate accuracy and loss for the epoch
+        epoch_loss = running_loss / total
+        epoch_acc = running_corrects.double() / total
 
-      train_losses.append(epoch_loss)
-      train_acc.append(epoch_acc)
+        train_losses.append(epoch_loss)
+        train_acc.append(epoch_acc)
 
-      # Validation phase
-      model.eval()  # Set model to evaluation mode
-      test_loss = 0.0
-      test_corrects = 0
-      test_total = 0
+        # Validation phase
+        model.eval()  # Set model to evaluation mode
+        test_loss = 0.0
+        test_corrects = 0
+        test_total = 0
 
-      all_preds = []
-      all_labels = []
-      all_prob = []
+        all_preds = []
+        all_labels = []
+        all_prob = []
 
-      with torch.no_grad():  # Disable gradient computation for validation
-          for test_batch in test_loader:
-              test_inputs, test_labels, _ = test_batch
+        with torch.no_grad():  # Disable gradient computation for validation
+            for test_batch in test_loader:
+                test_inputs, test_labels, _ = test_batch
 
-              test_outputs = model(test_inputs)
-              test_probabilities = F.softmax(test_outputs)
-              _, test_preds = torch.max(test_probabilities, 1)
+                test_outputs = model(test_inputs)
+                test_probabilities = F.softmax(test_outputs)
+                _, test_preds = torch.max(test_probabilities, 1)
 
-              test_loss += loss_fn(test_outputs, test_labels).item() * test_inputs.size(0)
-              test_corrects += torch.sum(test_preds == test_labels.data)
-              test_total += test_labels.size(0)
+                test_loss += loss_fn(test_outputs, test_labels).item() * test_inputs.size(0)
+                test_corrects += torch.sum(test_preds == test_labels.data)
+                test_total += test_labels.size(0)
 
-              all_preds.extend(test_preds.cpu().numpy())
-              all_prob.extend(test_probabilities.cpu().numpy())
-              all_labels.extend(test_labels.cpu().numpy())
+                all_preds.extend(test_preds.cpu().numpy())
+                all_prob.extend(test_probabilities.cpu().numpy())
+                all_labels.extend(test_labels.cpu().numpy())
 
-      test_epoch_loss = test_loss / test_total
-      test_epoch_acc = test_corrects.double() / test_total
+        test_epoch_loss = test_loss / test_total
+        test_epoch_acc = test_corrects.double() / test_total
 
-      test_losses.append(test_epoch_loss)
-      test_acc.append(test_epoch_acc)
-      all_prob = np.array(all_prob)
+        test_losses.append(test_epoch_loss)
+        test_acc.append(test_epoch_acc)
+        all_prob = np.array(all_prob)
 
 
-      print(f'Epoch {epoch+1}/{num_epochs} | Test Acc {test_epoch_acc} | Test Loss {test_epoch_loss}')
-      calibration_curve(all_labels, all_prob)
+        print(f'Epoch {epoch+1}/{num_epochs} | Test Acc {test_epoch_acc} | Test Loss {test_epoch_loss}')
+    return model
 
 def get_confusion_matrix(all_labels, all_preds):
   cm = confusion_matrix(all_labels, all_preds)
